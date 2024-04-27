81c2a78f66f2d52190d018a5e91779a0d30a1294
Bobomurod <bobomurodeshmamatov@gmail.com>
2024-04-27 18:14:01 +0500

    js/app.js js/index.html and js/style.css updates

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const home = document.querySelector('.home ')
 const electronics = document.querySelector('.electronics')
 const jewelery = document.querySelector('.jewelery')
 const men = document.querySelector('.men')
-const women = document.querySelector('.electronics')
+const women = document.querySelector('.women')
 const all = document.querySelector('.all')
 const korzinImg = document.querySelector('#number--1')
 const box = document.querySelector('.box')
@@ -43,30 +43,121 @@ const updateUI = (data) => {
     })
     jewelery.addEventListener('click', ()=>{
         box.innerHTML = ''
-        const result = data.slice(5,10)
+        const result = data.slice(4,8)
         result.forEach((item) => {
-            const {image, title, price} = item
-            box.innerHTML +=`
             
-            <div class="cart">
-            <div class="cart-img-box">
-              <img class="cart-img" src=${image} alt="img">
-            </div>
-            <h3 class="cart-title">${title}</h3>
-            <p class="rating">
-                <img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img">
-            </p>
-            <p class="price">${price}$ <del>109.95$</del> <span>24% Off</span></p>
-            <div class="cart-box">
-              <img class="cart-inner-img" 
-              src="https://exam-fourth-month.netlify.app/img/hearts.svg" alt="img">
-              <img class="cart-inner-img korzinka_btn" id="number--1" 
-              src="https://exam-fourth-month.netlify.app/img/korzinka_2.svg" alt="img">
-            </div>
-          </div>
-            `
+                const {image, title, price} = item
+                box.innerHTML +=`
+                
+                <div class="cart">
+                <div class="cart-img-box">
+                  <img class="cart-img" src=${image} alt="img">
+                </div>
+                <h3 class="cart-title">${title}</h3>
+                <p class="rating">
+                    <img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img">
+                </p>
+                <p class="price">${price}$ <del>109.95$</del> <span>24% Off</span></p>
+                <div class="cart-box">
+                  <img class="cart-inner-img" 
+                  src="https://exam-fourth-month.netlify.app/img/hearts.svg" alt="img">
+                  <img class="cart-inner-img korzinka_btn" id="number--1" 
+                  src="https://exam-fourth-month.netlify.app/img/korzinka_2.svg" alt="img">
+                </div>
+              </div>
+                `
+            
+           
+        })
+    }) 
+    electronics.addEventListener('click', ()=>{
+        box.innerHTML = ''
+        const result = data.slice(8,14)
+        console.log(result)
+        result.forEach((item) => {
+            // if(item.category === 'jewelery'){
+                const {image, title, price} = item
+                box.innerHTML +=`
+                
+                <div class="cart">
+                <div class="cart-img-box">
+                  <img class="cart-img" src=${image} alt="img">
+                </div>
+                <h3 class="cart-title">${title}</h3>
+                <p class="rating">
+                    <img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img">
+                </p>
+                <p class="price">${price}$ <del>109.95$</del> <span>24% Off</span></p>
+                <div class="cart-box">
+                  <img class="cart-inner-img" 
+                  src="https://exam-fourth-month.netlify.app/img/hearts.svg" alt="img">
+                  <img class="cart-inner-img korzinka_btn" id="number--1" 
+                  src="https://exam-fourth-month.netlify.app/img/korzinka_2.svg" alt="img">
+                </div>
+              </div>
+                `
+            
+           
         })
     })
     
+    women.addEventListener('click', ()=>{
+        box.innerHTML = ''
+        const result = data.slice(14,20)
+        console.log(result)
+        result.forEach((item) => {
+            // if(item.category === 'jewelery'){
+                const {image, title, price} = item
+                box.innerHTML +=`
+                
+                <div class="cart">
+                <div class="cart-img-box">
+                  <img class="cart-img" src=${image} alt="img">
+                </div>
+                <h3 class="cart-title">${title}</h3>
+                <p class="rating">
+                    <img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img">
+                </p>
+                <p class="price">${price}$ <del>109.95$</del> <span>24% Off</span></p>
+                <div class="cart-box">
+                  <img class="cart-inner-img" 
+                  src="https://exam-fourth-month.netlify.app/img/hearts.svg" alt="img">
+                  <img class="cart-inner-img korzinka_btn" id="number--1" 
+                  src="https://exam-fourth-month.netlify.app/img/korzinka_2.svg" alt="img">
+                </div>
+              </div>
+                `
+            
+           
+        })
+    })
+    men.addEventListener('click', ()=>{
+        box.innerHTML = ''
+        const result = data.slice(0,4)
+        console.log(result)
+        result.forEach((item) => {
+            // if(item.category === 'jewelery'){
+                const {image, title, price} = item
+                box.innerHTML +=`
+                
+                <div class="cart">
+                <div class="cart-img-box">
+                  <img class="cart-img" src=${image} alt="img">
+                </div>
+                <h3 class="cart-title">${title}</h3>
+                <p class="rating">
+                    <img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img"><img src="https://exam-fourth-month.netlify.app/img/star.svg" alt="img">
+                </p>
+                <p class="price">${price}$ <del>109.95$</del> <span>24% Off</span></p>
+                <div class="cart-box">
+                  <img class="cart-inner-img" 
+                  src="https://exam-fourth-month.netlify.app/img/hearts.svg" alt="img">
+                  <img class="cart-inner-img korzinka_btn" id="number--1" 
+                  src="https://exam-fourth-month.netlify.app/img/korzinka_2.svg" alt="img">
+                </div>
+              </div>
+                `
+        })
+    })
 }
 
